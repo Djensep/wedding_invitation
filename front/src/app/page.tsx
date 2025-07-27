bd24175ebbaef956a98c9258037b3bd262e2b3cd
Lee Djen Sep <leedjensep@gmail.com>
2025-07-28 00:56:40 +0500

    UPD ui input for range & radio

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -3,7 +3,7 @@ import { Input } from "@/shared/ui/Input/Input";
 export default function Home() {
     return (
         <div>
-            <Input name={""} />
+            <Input type="range" range={{ min: 10, max: 30 }} name={""} />
         </div>
     );
 }
--- front/src/shared/ui/Input/Input.tsx
+++ front/src/shared/ui/Input/Input.tsx
@@ -1,18 +1,22 @@
 "use client";
 
-import { ChangeEvent, CSSProperties, forwardRef, KeyboardEvent, useState } from "react";
+import { ChangeEvent, CSSProperties, forwardRef, KeyboardEvent, ReactNode } from "react";
 import styles from "./Input.module.css";
 
 type Props = {
     label?: string;
     name: string;
-    type?: "text" | "email" | "number" | "date";
+    type?: "text" | "email" | "number" | "date" | "radio" | "range";
     placeholder?: string;
     style?: CSSProperties;
     disabled?: boolean;
     showIcon?: boolean;
     error?: string;
     value?: string;
+    checked?: boolean;
+    range?: { min: number; max: number };
+    step?: number;
+    children?: ReactNode;
     onKeyDown?: (e: KeyboardEvent<HTMLInputElement>) => void;
     onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
 };
@@ -29,22 +33,29 @@ export const Input = forwardRef<HTMLInputElement, Props>(
             type = "text",
             showIcon = false,
             error,
+            checked,
+            step,
+            children,
+            range,
             onKeyDown,
             onChange,
             ...rest
         },
         ref,
     ) => {
-        const [isVisible, setIsVisible] = useState<boolean>(false);
+        const isRadio = type === "radio";
+        const isRange = type === "range";
 
         return (
             <div className={styles.inputContainer} style={style}>
-                {label && (
+                {label && !isRadio && (
                     <label htmlFor={name} className={styles.label}>
                         {label}
                     </label>
                 )}
+
                 {error && <p className={styles.errorText}>{error}</p>}
+
                 <div className={styles.inputWrapper}>
                     <input
                         id={name}
@@ -56,11 +67,24 @@ export const Input = forwardRef<HTMLInputElement, Props>(
                         onKeyDown={onKeyDown}
                         disabled={disabled}
                         ref={ref}
-                        className={`${styles.input} ${error ? styles.inputError : ""}`}
-                        style={{ paddingRight: showIcon ? "40px" : undefined }}
+                        checked={isRadio ? checked : undefined}
+                        className={`${styles.input} ${error ? styles.inputError : ""} ${isRadio ? styles.radio : ""}`}
+                        style={{
+                            paddingRight: showIcon ? "40px" : undefined,
+                        }}
+                        min={range?.min || 0}
+                        max={range?.max || 0}
+                        step={isRange ? step : undefined}
                         {...rest}
                     />
+                    {isRadio && (
+                        <label htmlFor={name} style={{ marginLeft: "8px", cursor: "pointer" }}>
+                            {children}
+                        </label>
+                    )}
                 </div>
+
+                {isRange && value && <div className={styles.rangeValue}>Текущее значение: {value}</div>}
             </div>
         );
     },
